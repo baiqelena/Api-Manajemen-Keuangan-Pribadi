682b1016fa2bd25677cf12e485dc8507c7bc3c93
elena <bqelen@gmail.com>
2025-12-25 01:50:41 +0800

    Tambah CRUD tabel hutang (create, update, delete, list)

---
+++ database/migrations/2025_12_24_151707_create_hutangs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('hutangs', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('hutangs');
+        Schema::create('hutang', function (Blueprint $table) {
+    $table->id();
+    $table->string('nama_pemberi_hutang');
+    $table->date('tanggal_pinjam');
+    $table->text('keterangan')->nullable();
+    $table->timestamps();
+});
+
+
+    }
+};
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HutangController;
+
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
 
+Route::middleware('auth:api')->group(function () {
+    Route::apiResource('hutang', HutangController::class);
+    Route::get('/hutang', [HutangController::class, 'index']);
+    Route::post('/hutang', [HutangController::class, 'create']);
+    Route::get('/hutang/{id}', [HutangController::class, 'detail']);
+    Route::put('/hutang/{id}', [HutangController::class, 'update']);
+    Route::delete('/hutang/{id}', [HutangController::class, 'destroy']);
+});
